c56ea926f0e0f82dd287983bab7aa6ba026326e7
Fan Ye <yefan1994@gmail.com>
2014-04-10 09:41:14 -0700

    test pg support

--- berc.py
+++ berc.py
@@ -6,37 +6,37 @@ from flask import Flask, request, session, g, redirect, url_for, abort, \
 app = Flask(__name__)
 
 app.config.update(dict(
-	# DATABASE=os.path.join(app.root_path, 'berc.db'),
+	DATABASE=os.path.join(app.root_path, 'berc.db'),
 	DEBUG=True,
 	SECRET_KEY='development',
 	USERNAME='admin',
-	PASSWORD='default',
-	SQLALCHEMY_DATABASE_URI=os.environ['DATABASE_URL']
+	PASSWORD='default'
 ))
-app.config.from_envvar('BERC_SETTINGS', silent=True)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+# app.config.from_envvar('BERC_SETTINGS', silent=True)
 db = SQLAlchemy(app)
 
-# def connect_db():
-# 	rv = sqlite3.connect(app.config['DATABASE'])
-# 	rv.row_factory = sqlite3.Row
-# 	return rv
+def connect_db():
+	rv = sqlite3.connect(app.config['DATABASE'])
+	rv.row_factory = sqlite3.Row
+	return rv
 
-# def get_db():
-# 	if not hasattr(g, 'sqlite_db'):
-# 		g.sqlite_db = connect_db()
-# 	return g.sqlite_db
+def get_db():
+	if not hasattr(g, 'sqlite_db'):
+		g.sqlite_db = connect_db()
+	return g.sqlite_db
 
-# @app.teardown_appcontext
-# def close_db(error):
-# 	if hasattr(g, 'sqlite_db'):
-# 		g.sqlite_db.close()
+@app.teardown_appcontext
+def close_db(error):
+	if hasattr(g, 'sqlite_db'):
+		g.sqlite_db.close()
 
-# def init_db():
-# 	with app.app_context():
-# 		db = get_db()
-# 		with app.open_resource('schema.sql', mode='r') as f:
-# 			db.cursor().executescript(f.read())
-# 		db.commit()
+def init_db():
+	with app.app_context():
+		db = get_db()
+		with app.open_resource('schema.sql', mode='r') as f:
+			db.cursor().executescript(f.read())
+		db.commit()
 
 @app.route('/')
 def home():
