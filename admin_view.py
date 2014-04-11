a326a7dcbb6b2bc14c53fa8ba1746d43d4173fac
Fan Ye <yefan1994@gmail.com>
2014-04-10 23:04:00 -0700

    subscriber view bug fixed

--- admin_view.py
+++ admin_view.py
@@ -1,9 +1,9 @@
 from flask.ext.admin import Admin, BaseView, expose
 from flask import render_template
-# from models import subscribed_user
+from models import subscribed_user
 
 class MyView(BaseView):
 	@expose('/')
 	def show_emails(self):
-		# entries = subscribed_user.query.all()
-		return render_template('show_emails.html')
+		entries = subscribed_user.query.all()
+		return render_template('show_emails.html', entries=entries)
