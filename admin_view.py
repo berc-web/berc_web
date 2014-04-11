e92acdf90c05324acb6874e7b500db7b9e516f20
Fan Ye <yefan1994@gmail.com>
2014-04-10 22:59:53 -0700

    test

--- admin_view.py
+++ admin_view.py
@@ -4,5 +4,5 @@ from models import subscribed_user
 class MyView(BaseView):
 	@expose('/')
 	def show_emails(self):
-		entries = subscribed_user.query.all()
-		return render_template('show_emails.html', entries=entries)
+		# entries = subscribed_user.query.all()
+		return render_template('show_emails.html')
