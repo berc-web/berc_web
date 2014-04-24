e655cc84b7529175af80776afa96901fee3ee72e
Fan Ye <yefan1994@gmail.com>
2014-04-23 20:19:50 -0700

    remove admin register

--- admin_view.py
+++ admin_view.py
@@ -36,27 +36,7 @@ class MyAdminIndexView(admin.AdminIndexView):
 		if login.current_user.is_authenticated() and login.current_user.is_admin():
 			return redirect(url_for('.index'))
 
-		link = '<p>Don\'t have an account? <a href="' + url_for('.register_view') + '">Click here to register.</a></p>'
 		self._template_args['form'] = form
-		self._template_args['link'] = link
-		return super(MyAdminIndexView, self).index()
-
-	@expose('/register/', methods=('GET', 'POST'))
-	def register_view(self):
-		form = RegistrationForm(request.form)
-		if helpers.validate_form_on_submit(form):
-			user = User()
-
-			form.populate_obj(user)
-
-			db.session.add(user)
-			db.session.commit()
-
-			login.login_user(user)
-			return redirect(url_for('.index'))
-		link = '<p>Already have an account? <a href="' + url_for('.login_view') + '">Click here to log in.</a></p>'
-		self._template_args['form'] = form
-		self._template_args['link'] = link
 		return super(MyAdminIndexView, self).index()
 
 	@expose('/logout/')
