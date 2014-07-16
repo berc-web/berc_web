e0205ce7d323ac615d8f22230fc3388790e04adf
cjzcpsyx <harrychen-cjz@hotmail.com>
2014-07-15 18:45:00 -0700

    update templates

--- application/__init__.py
+++ application/__init__.py
@@ -8,12 +8,13 @@ from flask.ext.user import login_required, current_user, SQLAlchemyAdapter
 from flask.ext.wtf.csrf import CsrfProtect
 from werkzeug import secure_filename
 from forms import AvatarForm
-#config Scss
+# config Scss
 from flask.ext.scss import Scss
 
 app = Flask(__name__)
 app.config.from_object('config_berc.Config')
-#Scss
+
+# Scss
 Scss(app)
 
 # Database
