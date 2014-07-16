1d546893f65e5cd871e91b57aff418215c6c4ac8
Fan Ye <yefan1994@gmail.com>
2014-07-15 17:49:30 -0700

    change the bucket name config to heroku env var

--- config_berc.py
+++ config_berc.py
@@ -52,6 +52,6 @@ class Config(object):
 	UPLOAD_FOLDER = '/static/upload/user_avatar/'
 	# DEFAULT_FILE_STORAGE = 'filesystem'
 
-	S3_BUCKET_NAME = 'eecc2015'
+	S3_BUCKET_NAME = os.environ.get('S3_BUCKET_NAME')
 	AWS_ACCESS_KEY_ID = 'AKIAJXPD4DCVTVKEQDXA'
 	AWS_SECRET_ACCESS_KEY = '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'
