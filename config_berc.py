77d77ab5733434ae310fa7b5f8ad44b4b6da112d
Fan Ye <yefan1994@gmail.com>
2014-07-20 17:25:00 -0700

    fix config bug

--- config_berc.py
+++ config_berc.py
@@ -52,8 +52,8 @@ class Config(object):
 	# DEFAULT_FILE_STORAGE = 'filesystem'
 
 	S3_UPLOAD_DIRECTORY = 'upload/user_avatar'
-	# S3_NEWS_IMAGE_DIR = 'upload/news_image'
+	S3_NEWS_IMAGE_DIR = 'upload/news_image'
 	S3_BUCKET_NAME = os.environ.get('S3_BUCKET_NAME')
-	S3_BUCKET_NAME = 'eecc2015-test'
+	# S3_BUCKET_NAME = 'eecc2015-test'
 	AWS_ACCESS_KEY_ID = 'AKIAJXPD4DCVTVKEQDXA'
 	AWS_SECRET_ACCESS_KEY = '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'
