d0595b9520ab9f2161c66ae2bc34e77e166ad33d
FanYe-rush <yefan1994@gmail.com>
2014-08-07 16:51:48 -0700

    Update config_berc.py

--- config_berc.py
+++ config_berc.py
@@ -53,5 +53,5 @@ class Config(object):
 
 	S3_UPLOAD_DIRECTORY = 'upload/user_avatar'
 	S3_BUCKET_NAME = os.environ.get('S3_BUCKET_NAME')
-	AWS_ACCESS_KEY_ID = 'AKIAJXPD4DCVTVKEQDXA'
-	AWS_SECRET_ACCESS_KEY = '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'
+	AWS_ACCESS_KEY_ID = ''
+	AWS_SECRET_ACCESS_KEY = ''
