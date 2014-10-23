244ebb93d9eda161911c486b7f814b8a740334e2
Fan Ye <yefan1994@gmail.com>
2014-10-22 22:32:45 -0700

    add competition start status

--- config_berc.py
+++ config_berc.py
@@ -59,5 +59,6 @@ class Config(object):
 	AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
 	AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 
+	COMPETATION_START = False
 	COMPETATION_CLOSED = False
 	LANGUAGE = "en"
